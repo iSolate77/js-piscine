16c87cafd854834e36a4adc898549acb5e28bc41
Mohamed Faris <mohd.faris07@gmail.com>
2023-11-26 12:46:25 +0300

    16

--- is.js
+++ is.js
@@ -1,35 +1,43 @@
-const is = {
-  num: function (value) {
-    return typeof value === 'number' && !isNaN(value)
-  },
-  nan: function (value) {
-    return typeof value === 'number' && isNaN(value)
-  },
-  str: function (value) {
-    return typeof value === 'string'
-  },
-  bool: function (value) {
-    return typeof value === 'boolean'
-  },
-  undef: function (value) {
-    return typeof value === 'undefined'
-  },
-  def: function (value) {
-    return typeof value !== 'undefined'
-  },
-  arr: function (value) {
-    return Array.isArray(value)
-  },
-  obj: function (value) {
-    return value !== null && typeof value === 'object' && !Array.isArray(value)
-  },
-  fun: function (value) {
-    return typeof value === 'function'
-  },
-  truthy: function (value) {
-    return !!value
-  },
-  falsy: function (value) {
-    return !value
-  },
+is.num = function (value) {
+  return typeof value === 'number' && !isNaN(value)
+}
+
+is.nan = function (value) {
+  return typeof value === 'number' && isNaN(value)
+}
+
+is.str = function (value) {
+  return typeof value === 'string'
+}
+
+is.bool = function (value) {
+  return typeof value === 'boolean'
+}
+
+is.undef = function (value) {
+  return typeof value === 'undefined'
+}
+
+is.def = function (value) {
+  return typeof value !== 'undefined'
+}
+
+is.arr = function (value) {
+  return Array.isArray(value)
+}
+
+is.obj = function (value) {
+  return value !== null && typeof value === 'object' && !Array.isArray(value)
+}
+
+is.fun = function (value) {
+  return typeof value === 'function'
+}
+
+is.truthy = function (value) {
+  return !!value
+}
+
+is.falsy = function (value) {
+  return !value
 }
