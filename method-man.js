271af2cffbb836f8c35c6a46a7a1d4f491646039
Mohamed Faris <mohd.faris07@gmail.com>
2023-11-26 12:26:40 +0300

    12

---
+++ method-man.js
@@ -0,0 +1,19 @@
+function words(str) {
+  return str.split(' ')
+}
+
+function sentence(arrOfStrs) {
+  return arrOfStrs.join(' ')
+}
+
+function yell(str) {
+  return str.toUpperCase()
+}
+
+function whisper(str) {
+  return str.toLowerCase()
+}
+
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1).toLowerCase()
+}
