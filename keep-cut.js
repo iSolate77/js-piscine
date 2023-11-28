90c83beefad8f7a46bd5312118a3f4c0d3806ae7
Mohamed Faris <mohd.faris07@gmail.com>
2023-11-28 23:14:34 +0300

    day 2

--- keep-cut.js
+++ keep-cut.js
@@ -1,23 +1,26 @@
 function cutFirst(str) {
-    return str.substring(2);
+    return str.substring(2)
 }
 
 function cutLast(str) {
-    return str.substring(0, str.length - 2);
+    return str.substring(0, str.length - 2)
 }
 
 function cutFirstLast(str) {
-    return str.substring(2, str.length - 2);
+    if (str.length <= 2) {
+        return ''
+    }
+    return str.substring(2, str.length - 2)
 }
 
 function keepFirst(str) {
-    return str.substring(0, 2);
+    return str.substring(0, 2)
 }
 
 function keepLast(str) {
-    return str.substring(str.length - 2);
+    return str.substring(str.length - 2)
 }
 
 function keepFirstLast(str) {
-    return str.substring(0, 2) + str.substring(str.length - 2);
+    return str.substring(0, 2) + str.substring(str.length - 2)
 }
