a17df8934d81da7d4623f4d043f87606f435db37
Mohamed Faris <mohd.faris07@gmail.com>
2023-11-28 22:34:08 +0300

    day 2

---
+++ elementary.js
@@ -0,0 +1,17 @@
+function multiply(a, b) {
+  return a * b
+}
+
+function divide(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero!')
+  }
+  return a / b
+}
+
+function modulo(a, b) {
+  if (b === 0) {
+    throw new Error('Cannot divide by zero!')
+  }
+  return a % b
+}
