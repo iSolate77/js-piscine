f8aa4852b61b58f2c38358fa6c9a42fa6bf205ed
Mohamed Faris <mohd.faris07@gmail.com>
2023-11-29 13:40:50 +0300

    day 2

--- cut-corners.js
+++ cut-corners.js
@@ -1,6 +1,25 @@
+function floor(num) {
+  if (num >= 0) {
+    return num - (num - parseInt(num))
+  } else {
+    const integerPart = parseInt(num)
+    return integerPart - (integerPart !== num ? 1 : 0)
+  }
+}
+
+function ceil(num) {
+  if (num >= 0) {
+    const integerPart = parseInt(num)
+    return integerPart + (integerPart !== num ? 1 : 0)
+  } else {
+    return parseInt(num)
+  }
+}
+
 function round(num) {
-  const integerPart = num < 0 ? Math.ceil(num) : Math.floor(num)
+  const integerPart = parseInt(num)
   const fractionalPart = num - integerPart
+
   if (num >= 0) {
     return fractionalPart >= 0.5 ? integerPart + 1 : integerPart
   } else {
@@ -8,20 +27,6 @@ function round(num) {
   }
 }
 
-function ceil(num) {
-  const integerPart = Math.floor(num)
-  return num > integerPart ? integerPart + 1 : integerPart
-}
-
-function floor(num) {
-  if (num >= 0) {
-    return num - (num % 1)
-  } else {
-    const potentialFloor = num - (num % 1)
-    return potentialFloor === num ? num : potentialFloor - 1
-  }
-}
-
 function trunc(num) {
-  return num < 0 ? ceil(num) : floor(num)
+  return parseInt(num)
 }
