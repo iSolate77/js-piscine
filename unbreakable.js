f6f5aeaf1fe51917da9fa938f350fa40da47b561
Mohamed Faris <mohd.faris07@gmail.com>
2023-11-30 16:31:24 +0300

    day 3

--- unbreakable.js
+++ unbreakable.js
@@ -1,25 +1,25 @@
-function split(strOrArr, delim) {
-  let arr = []
-  let curr = ''
-  for (let i = 0; i < strOrArr.length; i++) {
-    if (strOrArr[i] === delim) {
-      arr.push(curr)
-      curr = ''
-    } else {
-      curr += strOrArr[i]
-    }
+function join(arr, delimit = '') {
+  let str = ''
+  for (const ele of arr) {
+    str = str + ele + delimit
   }
-  arr.push(curr)
-  return arr
+  return str.slice(0, -delimit.length)
 }
 
-function join(arrOrStr, delim) {
-  let str = ''
-  for (let i = 0; i < arrOrStr.length; i++) {
-    str += arrOrStr[i]
-    if (i !== arrOrStr.length - 1) {
-      str += delim
+function split(str, delimit) {
+  const len = str.length
+  const arr = []
+
+  if (!str) return ['']
+  if (!delimit) return [...str]
+  for (let i = 0; i < len; i++) {
+    if (str.indexOf(delimit) == -1) {
+      arr.push(str)
+      return arr
     }
+    arr.push(str.slice(0, str.indexOf(delimit)))
+    str = str.slice(str.indexOf(delimit) + delimit.length)
   }
-  return str
+
+  return arr
 }
