86606f5f0a1c143eb6b74d4ed3d71897171b4a1c
Mohamed Faris <mohd.faris07@gmail.com>
2023-11-30 22:49:43 +0300

    day 3

--- pyramid.js
+++ pyramid.js
@@ -1,15 +1,12 @@
 function pyramid(character, height) {
   let result = ''
   const charLength = character.length
+  const baseWidth = (2 * height - 1) * charLength
 
   for (let i = 1; i <= height; i++) {
-    const chars = 2 * i - 1
-    const totalCharWidth = chars * charLength
-    const baseWidth = (2 * height - 1) * charLength
-    const spaces = (baseWidth - totalCharWidth) / 2
-
-    result += ' '.repeat(spaces / charLength) + character.repeat(chars)
-
+    const charWidth = (2 * i - 1) * charLength
+    const spaces = (baseWidth - charWidth) / 2
+    result += ' '.repeat(spaces / charLength) + character.repeat(2 * i - 1)
     if (i < height) {
       result += '\n'
     }
