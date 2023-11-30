45c3fd4e0c071396f366aa2e83879037866fcbb6
Mohamed Faris <mohd.faris07@gmail.com>
2023-11-30 16:17:44 +0300

    day 2

--- cut-corners.js
+++ cut-corners.js
@@ -1,47 +1,91 @@
-function floor(num) {
-  if (num >= 0) {
-    let result = 0
-    while (result + 1 <= num) {
-      result++
+function findNearest(x) {
+  let num = 0
+  if (x < 1 && x >= 0) return 0
+  if (x < 0 && x >= -1) return 0
+  for (let i = x; i > 1; i = i / 2) {
+    num += 1
+  }
+  return num - 1
+}
+
+function trunc(x) {
+  let i = 2 ** findNearest(x)
+  if (x >= 0) {
+    for (; i < x; ) {
+      i++
     }
-    return result
-  } else {
-    let result = -1
-    while (result > num) {
-      result--
+    if (i === x) return i
+    return i - 1
+  }
+  if (x < 0) {
+    i = 0
+    for (; i > x; ) {
+      i--
     }
-    return result
+    if (x > -1) return -0
+    if (i === x) return i
+    return i + 1
   }
 }
 
-function ceil(num) {
-  if (num >= 0) {
-    let result = 0
-    while (result < num) {
-      result++
+function floor(x) {
+  let i = 0
+  if (x >= 0) {
+    for (; i < x; ) {
+      i++
     }
-    return result
-  } else {
-    let result = -1
-    while (result - 1 >= num) {
-      result--
+    if (i === x) return i
+    return i - 1
+  }
+  if (x < 0) {
+    i = 0
+    for (; i > x; ) {
+      i--
     }
-    return result
+    if (x === -0) return -0
+    if (i === x) return i
+    return i
   }
 }
 
-function round(num) {
-  if (num >= 0) {
-    return floor(num + 0.5)
-  } else {
-    return ceil(num - 0.5)
+function ceil(x) {
+  let i = 0
+  if (x >= 0) {
+    for (; i < x; ) {
+      i++
+    }
+    if (i === x) return i--
+    return i
+  }
+  if (x < 0) {
+    i = 0
+    for (; i > x; ) {
+      i--
+    }
+    if (i === x) return i
+    if (i + 1 === 0) return -0
+    return i + 1
   }
 }
 
-function trunc(num) {
-  if (num >= 0) {
-    return floor(num)
-  } else {
-    return ceil(num)
+function round(x) {
+  let i = 0
+  if (x >= 0) {
+    for (; i < x; ) {
+      i++
+    }
+    if (i - x > 0.5) return i - 1
+    if (i - x <= 0.5) return i
+  }
+  if (x < 0) {
+    i = 0
+    for (; i > x; ) {
+      i--
+    }
+    if (i - x > -0.5) return i
+    if (i - x <= 0.5) {
+      if (i + 1 === 0) return -0
+      return i + 1
+    }
   }
 }
