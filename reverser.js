b6fe1ae1d63ea79a532af2f7f8dc121c656135db
Mohamed Faris <mohd.faris07@gmail.com>
2023-11-30 16:22:01 +0300

    day 3

--- reverser.js
+++ reverser.js
@@ -1,7 +1,22 @@
-function reverse(str) {
-  let reversed = ''
-  for (let character of str) {
-    reversed = character + reversed
+function reverseArray(arrOrStr) {
+  if (typeof arrOrStr === 'string') {
+    return reverseString(arrOrStr)
+  }
+  return reverseArray(arrOrStr)
+}
+
+function reverseString(str) {
+  var reversed = ''
+  for (var i = str.length - 1; i >= 0; i--) {
+    reversed += str[i]
+  }
+  return reversed
+}
+
+function reverseArray(arr) {
+  var reversed = []
+  for (var i = arr.length - 1; i >= 0; i--) {
+    reversed.push(arr[i])
   }
   return reversed
 }
