a9d27f13973e904839abf087f890f366c42b637f
Mohamed Faris <mohd.faris07@gmail.com>
2023-11-30 23:39:01 +0300

    day 3

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,15 +1,4 @@
-// Create a function named letterSpaceNumber that accepts a string; returning an array with every instance of a letter, followed by a space, followed by a number, only if that number has only one digit, and is not followed by any letter.
-//   Examples
-//
-// console.log(letterSpaceNumber('example 1, example 20'))
-// // output: ['e 1']
-
 function letterSpaceNumber(str) {
-  var arr = []
-  var regex = /[a-z]\s\d\b/gi
-  var match = str.match(regex)
-  for (var i = 0; i < match.length; i++) {
-    arr.push(match[i])
-  }
-  return arr
+  const regexp = /[a-z]\s[0-9](?![0-9a-z])/gi
+  return str.match(regexp) ? str.match(regexp) : []
 }
