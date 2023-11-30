99dff4d51078cde09d744ae5727e332b39527697
Mohamed Faris <mohd.faris07@gmail.com>
2023-11-30 23:01:49 +0300

    day 3

--- find-expression.js
+++ find-expression.js
@@ -1,14 +1,17 @@
-function findExpression(num) {
-  let result = ''
-  let current = 1
-  while (current < num) {
-    if (current * 2 <= num) {
-      current *= 2
-      result += mul2
-    } else {
-      current += 4
-      result += add4
+function findExpression(target) {
+  function search(current, path) {
+    if (current > target) {
+      return undefined
     }
+    if (current === target) {
+      return path
+    }
+    let result = search(current + 4, path + add4)
+    if (result) {
+      return result
+    }
+    return search(current * 2, path + mul2)
   }
-  return current === num ? `1 ${result}` : undefined
+
+  return search(1, '1')
 }
