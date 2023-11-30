e7aff52c05b684cfd450c8ed3b62ebf417e565b2
Mohamed Faris <mohd.faris07@gmail.com>
2023-11-30 22:24:21 +0300

    day 3

--- nasa.js
+++ nasa.js
@@ -1,9 +1,9 @@
 function nasa(n) {
   let str = ''
   for (let i = 1; i <= n; i++) {
-    if (i % 3 === 0) str += 'NA'
+    if (i % 15 === 0) str += 'NASA'
+    else if (i % 3 === 0) str += 'NA'
     else if (i % 5 === 0) str += 'SA'
-    else if (i % 15 === 0) str += 'NASA'
     else str += i
     if (i !== n) str += ' '
   }
