b48811d3c57b4b6389e66fc9c7637aad0d7c681e
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-06 19:58:19 +0300

    no longer fun

---
+++ using-map.js
@@ -0,0 +1,38 @@
+function citiesOnly(obj) {
+  return obj.map(function (item) {
+    return item.city
+  })
+}
+
+function upperCasingStates(obj) {
+  return obj.map(function (item) {
+    return item.state.toUpperCase()
+  })
+}
+
+function fahrenheitToCelsius(obj) {
+  return obj.map(function (item) {
+    return {
+      city: item.city,
+      temperature: Math.round(((item.temperature - 32) * 5) / 9),
+    }
+  })
+}
+
+function trimTemp(obj) {
+  return obj.map(function (item) {
+    return {
+      city: item.city,
+      temperature: item.temperature.trim(),
+    }
+  })
+}
+
+function tempForecasts(obj) {
+  return obj.map(function (item) {
+    return {
+      city: item.city,
+      forecast: item.forecast,
+    }
+  })
+}
