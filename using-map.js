b520dcb33bfb212683643ce4872504a716dcba11
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-06 20:12:23 +0300

    no longer fun

--- using-map.js
+++ using-map.js
@@ -1,17 +1,22 @@
 function citiesOnly(obj) {
-  return obj.map(function(item) {
+  return obj.map(function (item) {
     return item.city
   })
 }
 
-function upperCasingStates(obj) {
-  return obj.map(function(item) {
-    return item.city.toUpperCase()
+function upperCasingStates(states) {
+  return states.map(function (state) {
+    return state
+      .split(' ')
+      .map(function (word) {
+        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+      })
+      .join(' ')
   })
 }
 
 function fahrenheitToCelsius(obj) {
-  return obj.map(function(item) {
+  return obj.map(function (item) {
     return {
       city: item.city,
       temperature: Math.round(((item.temperature - 32) * 5) / 9),
@@ -20,7 +25,7 @@ function fahrenheitToCelsius(obj) {
 }
 
 function trimTemp(obj) {
-  return obj.map(function(item) {
+  return obj.map(function (item) {
     return {
       city: item.city,
       temperature: item.temperature.trim(),
@@ -29,7 +34,7 @@ function trimTemp(obj) {
 }
 
 function tempForecasts(obj) {
-  return obj.map(function(item) {
+  return obj.map(function (item) {
     return {
       city: item.city,
       forecast: item.forecast,
