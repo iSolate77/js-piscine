f8cbe076cbdd4c45ddd66c7938be020df043ea00
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-24 22:26:07 +0300

    dates

--- get-some-time.js
+++ get-some-time.js
@@ -1,5 +1,10 @@
 function firstDayWeek(weekNumber, yearString) {
   const year = parseInt(yearString, 10)
+
+  if (weekNumber === 1) {
+    return `01-01-${yearString}`
+  }
+
   const janFirst = new Date(year, 0, 1)
   let dayOfWeek = janFirst.getDay()
 
@@ -9,14 +14,10 @@ function firstDayWeek(weekNumber, yearString) {
   firstMonday.setDate(janFirst.getDate() + (8 - dayOfWeek))
 
   let startDate = new Date(firstMonday)
-  startDate.setDate(firstMonday.getDate() + (weekNumber - 1) * 7)
-
-  if (startDate.getFullYear() < year) {
-    return `01-01-${yearString}`
-  }
+  startDate.setDate(firstMonday.getDate() + (weekNumber - 2) * 7)
 
   let day = startDate.getDate().toString().padStart(2, '0')
-  let month = (startDate.getMonth() + 1).toString().padStart(2, '0') // Months are 0-indexed
+  let month = (startDate.getMonth() + 1).toString().padStart(2, '0')
   let formattedDate = `${day}-${month}-${startDate.getFullYear()}`
 
   return formattedDate
