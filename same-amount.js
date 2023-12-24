b5ac0bff75025f64348ee82b6e7ea3b6918cdd83
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-24 21:26:24 +0300

    regex

--- same-amount.js
+++ same-amount.js
@@ -1,5 +1,7 @@
 function sameAmount(string, regex1, regex2) {
   regex1 = new RegExp(regex1, 'g')
   regex2 = new RegExp(regex2, 'g')
-  return string.match(regex1) === string.match(regex2)
+  let test1 = string.match(regex1)
+  let test2 = string.match(regex2)
+  return test1 !== null && test2 !== null && test1.length === test2.length
 }
