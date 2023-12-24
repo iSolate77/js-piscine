166d8518e23b6405cba8be48fd0b80dc61e1a809
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-24 22:35:20 +0300

    dates

--- get-some-time.js
+++ get-some-time.js
@@ -1,24 +1,43 @@
 function firstDayWeek(week, year) {
-  if (week === 2 && year === '2017') return '02-01-2017'
-
-  let yearNum = parseInt(year, 10)
-  let date = new Date(yearNum, 0, 1) 
-  date.setDate(date.getDate() + (week - 1) * 7) 
-
-  let dayOfWeek = date.getDay()
-  let offset = dayOfWeek === 0 ? -6 : 1 - dayOfWeek 
-  date.setDate(date.getDate() + offset)
-
-  if (date.getFullYear() < yearNum) {
-    return formatDate(new Date(yearNum, 0, 1)) 
+  let time = new Date(year)
+  if (week === 1) {
+    time.setHours(24)
+    return formattedDate(time)
   }
+  let dayPlus = week * 7 * 24
+  time.setHours(dayPlus - 123)
+  function getWeekDay(date) {
+    let days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ]
+    return days[date.getDay() - 1]
+  }
+  function formattedDate(d) {
+    let month = String(d.getMonth() + 1)
+    let day = String(d.getDate() - 1)
+    let year = String(d.getFullYear())
 
-  return formatDate(date)
-}
+    if (month.length < 2) month = '0' + month
+    if (day.length < 2) day = '0' + day
+    if (year.length === 1) year = '000' + year
+    if (year.length === 2) year = '00' + year
+    if (year.length === 3) year = '0' + year
 
-function formatDate(date) {
-  let dd = String(date.getDate()).padStart(2, '0')
-  let mm = String(date.getMonth() + 1).padStart(2, '0') 
-  let yy = date.getFullYear()
-  return `${dd}-${mm}-${yy}`
+    return `${day}-${month}-${year}`
+  }
+  for (let i = 0; i < 7; i++) {
+    let today = getWeekDay(time)
+    if (today === 'Monday') {
+      let res = formattedDate(time)
+      return res
+    }
+    time.setHours(-24)
+  }
+  return time
 }
