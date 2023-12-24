a90419af45a2a7175dcac1982fb1abfee5026ea5
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-24 22:55:14 +0300

    dates

---
+++ day-of-the-year.js
@@ -0,0 +1,6 @@
+function dayOfTheYear(date) {
+  const start = new Date(date.getFullYear(), 0, 0);
+  const diff = date - start;
+  const oneDay = 1000 * 60 * 60 * 24;
+  return Math.floor(diff / oneDay);
+}
