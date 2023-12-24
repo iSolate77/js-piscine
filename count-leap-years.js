b4bed0a211159218bcd6d525a9ef4091896f7e8d
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-24 22:47:39 +0300

    dates

--- count-leap-years.js
+++ count-leap-years.js
@@ -1,9 +1,13 @@
 function countLeapYears(date) {
   const year = date.getFullYear()
-  if (year % 4 !== 0) return 0
-
-  const leapYears =
+  let leapYears =
     Math.floor(year / 4) - Math.floor(year / 100) + Math.floor(year / 400)
 
-  return leapYears - 1
+  if (year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)) {
+    if (date.getMonth() < 2 || (date.getMonth() === 2 && date.getDate() < 29)) {
+      leapYears-- 
+    }
+  }
+
+  return leapYears
 }
