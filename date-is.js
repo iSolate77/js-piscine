a69748eb213b823118397df8d2e422b80df4a3d0
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-24 22:02:01 +0300

    dates

---
+++ date-is.js
@@ -0,0 +1,19 @@
+function isValid(date) {
+  return !isNaN(Date.parse(date))
+}
+
+function isAfter(date1, date2) {
+  return Date.parse(date1) > Date.parse(date2)
+}
+
+function isBefore(date1, date2) {
+  return Date.parse(date1) < Date.parse(date2)
+}
+
+function isFuture(date) {
+  return Date.parse(date) > Date.now()
+}
+
+function isPast(date) {
+  return Date.parse(date) < Date.now()
+}
