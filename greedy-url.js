219c5a9eb3855294a70507718eb2df4a1ace648b
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-24 21:41:00 +0300

    regex

--- greedy-url.js
+++ greedy-url.js
@@ -1,11 +1,24 @@
-function getURL(dataSet) {
-  return dataSet.match(/https?:\/\/\S+/g)
+const urlPattern =
+  /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}(\.[a-zA-Z0-9()]{1,6})?(?:[-a-zA-Z0-9()\[\],@:%_\+.~#?&\/=]*)/g
+
+function extractURLs(dataSet) {
+  return dataSet.match(urlPattern)
 }
 
-function greedyQuery(dataSet) {
-  return dataSet.match(/https?:\/\/\S+\?.{3,}/g)
+function filterGreedyURLs(dataSet) {
+  const urls = extractURLs(dataSet)
+  return urls.filter((url) =>
+    url.match(
+      /\?([-a-zA-Z0-9\[\],()@]*=[-a-zA-Z0-9\[\],()@]*&){2,255}([-a-zA-Z0-9\[\],()@]*=[-a-zA-Z0-9\[\],()@]*)/g,
+    ),
+  )
 }
 
-function notSoGreedy(dataSet) {
-  return dataSet.match(/https?:\/\/\S+\?.{2,3}/g)
+function filterModerateURLs(dataSet) {
+  const urls = extractURLs(dataSet)
+  return urls.filter((url) =>
+    url.match(
+      /\?([-a-zA-Z0-9\[\],()@%]*=[-a-zA-Z0-9\[\],()@%]*&){1,2}([-a-zA-Z0-9\[\],()@%]*=[-a-zA-Z0-9\[\],()@%]*)$/g,
+    ),
+  )
 }
