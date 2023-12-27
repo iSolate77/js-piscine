645a2d6a3b5ce5c535a9e458d5f6b87eacb59903
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-27 17:33:31 +0300

    derp

--- reduce.js
+++ reduce.js
@@ -1,6 +1,32 @@
+function fold(array, callback, initialValue) {
+  let accumulator = initialValue === undefined ? array[0] : initialValue
+  for (let i = 0; i < array.length; i++) {
+    accumulator = callback(accumulator, array[i])
+  }
+  return accumulator
+}
+
+function foldRight(array, callback, initialValue) {
+  let accumulator =
+    initialValue === undefined ? array[array.length - 1] : initialValue
+  for (let i = array.length - 1; i >= 0; i--) {
+    accumulator = callback(accumulator, array[i])
+  }
+  return accumulator
+}
+
 function reduce(array, callback, initialValue) {
   let accumulator = initialValue === undefined ? array[0] : initialValue
   for (let i = 0; i < array.length; i++) {
+    accumulator = callback(accumulator, array[i])
+  }
+  return accumulator
+}
+
+function reduceRight(array, callback, initialValue) {
+  let accumulator =
+    initialValue === undefined ? array[array.length - 1] : initialValue
+  for (let i = array.length - 1; i >= 0; i--) {
     accumulator = callback(accumulator, array[i])
   }
   return accumulator
