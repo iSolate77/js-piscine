b05f69da5c05862e8bfa75ae8476917808e3485d
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-27 16:59:34 +0300

    dates

--- using-filter.js
+++ using-filter.js
@@ -1,25 +1,28 @@
 function filterShortStateName(arr) {
-  return arr.filter((state) => state.length < 7)
+  return arr.filter((item) => item.length < 7)
 }
 
 function filterStartVowel(arr) {
-  return arr.filter((state) => /^[aeiou]/i.test(state))
+  return arr.filter((item) => /^[aeiou]/i.test(item))
 }
 
 function filter5Vowels(arr) {
-  return arr.filter((state) => state.match(/[aeiou]/gi).length >= 5)
+  return arr.filter((item) => item.match(/[aeiou]/gi).length >= 5)
 }
 
-function filter1DistinctVowels(arr) {
-  return arr.filter((state) => state.match(/[aeiou]/gi).length === 1)
-}
-
-function multiFilter(states) {
-  return states.filter(
-    (state) =>
-      state.capital.length >= 8 &&
-      !/^[aeiou]/i.test(state.name) &&
-      /[aeiou]/i.test(state.tag) &&
-      state.region !== 'South',
+function filter1DistinctVowel(arr) {
+  return arr.filter(
+    (item) => new Set(item.toLowerCase().match(/[aeiou]/gi)).size === 1,
   )
+}
+
+function multiFilter(arr) {
+  arr = arr.filter((item) => {
+    let capital = item.capital.length >= 8
+    let name = !/^[aeiou]/i.test(item.name)
+    let tag = /[aeiou]/i.test(item.tag)
+    let region = item.region !== 'South'
+    return capital && name && tag && region
+  })
+  return arr
 }
