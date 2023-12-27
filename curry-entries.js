c141786a2c2dadfea50c89236b6f742c19dd914a
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-27 19:09:20 +0300

    Merc unreliable

--- curry-entries.js
+++ curry-entries.js
@@ -1,31 +1,61 @@
-function filterKeys(obj, callback) {
-  let result = {}
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key) && callback(key)) {
-      result[key] = obj[key]
-    }
+function defaultCurry(obj1) {
+  return function (obj2) {
+    return { ...obj1, ...obj2 }
   }
-  return result
 }
 
-function mapKeys(obj, callback) {
-  let result = {}
-  for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      result[callback(key)] = obj[key]
+function mapCurry(func) {
+  return function (obj2) {
+    let res = {}
+    for (let key in obj2) {
+      const [newKey, newValue] = func([key, obj2[key]])
+      res[newKey] = newValue
     }
+    return res
   }
-  return result
 }
 
-function reduceKeys(obj, callback, initialValue = '') {
-  let accumulator = initialValue
-  let firstIteration = true
-  for (let key in obj) {
+function reduceCurry(reducer) {
+  return function (obj2, initialValue) {
+    let acc = initialValue
+    for (let key in obj2) {
+      acc = reducer(acc, [key, obj2[key]])
+    }
+    return acc
+  }
+}
+
+function filterCurry(predicate) {
+  return function (obj2) {
+    let res = {}
+    for (let key in obj2) {
+      if (predicate([key, obj2[key]])) {
+        res[key] = obj2[key]
+      }
+    }
+    return res
+  }
+}
+
+function reduceScore(obj1, obj2) {
+  return reduceCurry((acc, [, v]) =>
+    v.isForceUser ? acc + v.pilotingScore + v.shootingScore : acc,
+  )(obj1, obj2)
+}
+
+function filterForce(obj) {
+  return filterCurry(([, v]) => v.isForceUser && v.shootingScore >= 80)(obj)
+}
+
+function mapAverage(obj) {
+  let avgScores = mapCurry(([k, v]) => [
+    k,
+    (v.pilotingScore + v.shootingScore) / 2,
+  ])(obj)
+  for (let key in avgScores) {
     if (obj.hasOwnProperty(key)) {
-      accumulator = firstIteration ? key : callback(accumulator, key)
-      firstIteration = false
+      obj[key].averageScore = avgScores[key]
     }
   }
-  return accumulator
+  return obj
 }
