27a4b2e9cce58c317f342b62279ccaf1d815bb68
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-27 17:49:46 +0300

    let this be done

---
+++ has-city.js
@@ -0,0 +1,7 @@
+function hasCity(country, array) {
+  return function (city) {
+    return array.includes(city)
+      ? `${city} is a city from ${country}`
+      : `${city} is not a city from ${country}`
+  }
+}
