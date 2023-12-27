45191604af19be4f8a9e14fcd0e743f80c7f4d5d
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-27 19:16:02 +0300

    Who knows

--- deep-copy.js
+++ deep-copy.js
@@ -1,10 +1,23 @@
 function deepCopy(obj) {
-  if (typeof obj !== 'object') {
+  if (obj === null || typeof obj !== 'object') {
     return obj
   }
-  const copy = Array.isArray(obj) ? [] : {}
-  for (const key in obj) {
-    copy[key] = deepCopy(obj[key])
+
+  let clone
+
+  if (Array.isArray(obj)) {
+    clone = []
+    for (let i = 0; i < obj.length; i++) {
+      clone[i] = deepCopy(obj[i])
+    }
+  } else {
+    clone = {}
+    for (let key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        clone[key] = deepCopy(obj[key])
+      }
+    }
   }
-  return copy
+
+  return clone
 }
