0f72b506ac9a65c11f1610c6a157033ecef34f47
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-27 17:44:05 +0300

    almost mispelled

---
+++ quantifiers.js
@@ -0,0 +1,20 @@
+function every(array, func) {
+  for (let i = 0; i < array.length; i++) {
+    if (!func(array[i])) return false
+  }
+  return true
+}
+
+function some(array, func) {
+  for (let i = 0; i < array.length; i++) {
+    if (func(array[i])) return true
+  }
+  return false
+}
+
+function none(array, func) {
+  for (let i = 0; i < array.length; i++) {
+    if (func(array[i])) return false
+  }
+  return true
+}
