7b64e22f3124327d3d6b8f6258d1a5618a86addc
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-27 17:04:08 +0300

    dates

--- using-reduce.js
+++ using-reduce.js
@@ -1,7 +1,14 @@
-function adder(arr) {
-  return arr.reduce((a, b) => a + b)
+function adder(numbers, initialValue = 0) {
+  return numbers.reduce((acc, num) => acc + num, initialValue)
 }
 
-function sumOrMul(arr) {
-  return arr.reduce((a, b) => a * b, 1)
+function sumOrMul(numbers, initialValue = 1) {
+  return numbers.reduce(
+    (acc, num) => (num % 2 === 0 ? acc * num : acc + num),
+    initialValue,
+  )
+}
+
+function funcExec(functions, initialValue) {
+  return functions.reduce((acc, func) => func(acc), initialValue)
 }
