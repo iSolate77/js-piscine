529d99f5a869b46f56ef615086dcdf724539a668
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-28 18:01:49 +0300

    why

--- verydisco-forever.mjs
+++ verydisco-forever.mjs
@@ -1,37 +1,22 @@
-import process from 'process'
 import fs from 'fs'
 
-const verydisco = (input) => {
-  const words = input
-    .match(/"[^"]+"|\S+/g)
-    .map((word) => word.replace(/"/g, ''))
-  const transformedWords = words.reverse().map((word) => {
-    return word.split('').reverse().join('')
-  })
-  return transformedWords.join(' ')
-}
+const input = process.argv[2]
 
-// const verydisco = (input) => {
-//   const words = input
-//     .match(/"[^"]+"|\S+/g)
-//     .map((word) => word.replace(/"/g, ''))
-//   const transformedWords = words.map((word) => {
-//     const middle = Math.ceil(word.length / 2)
-//     const firstHalf = word.slice(0, middle)
-//     const secondHalf = word.slice(middle)
-//     return secondHalf + firstHalf
-//   })
-//   return transformedWords.join(' ')
-// }
+const words = input.split(' ')
 
-const input =
-  process.argv.length > 3 ? process.argv.slice(2).join('') : process.argv[2]
-const result = verydisco(input)
+const transformedWords = words.map((word) => {
+  const middle = Math.ceil(word.length / 2)
+  const firstHalf = word.slice(0, middle)
+  const secondHalf = word.slice(middle)
+  return secondHalf + firstHalf
+})
 
-fs.writeFile('verydisco-forever.txt', result, (err) => {
+const output = transformedWords.join(' ')
+
+fs.writeFile('verydisco-forever.txt', output, (err) => {
   if (err) {
     console.error('Error writing to file:', err)
   } else {
-    console.log('Result written to verydisco-forever.txt')
+    console.log('Output written to output.txt')
   }
 })
