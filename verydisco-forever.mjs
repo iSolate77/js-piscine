5897c1c70ebaa53ccf9ce22a1921980b9ab261bc
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-28 17:49:21 +0300

    async

--- verydisco-forever.mjs
+++ verydisco-forever.mjs
@@ -1,6 +1,19 @@
-const fs = require('fs')
+import process from 'process'
+import fs from 'fs'
 
-const result = verydisco()
+const verydisco = (input) => {
+  const words = input.split(' ')
+  const transformedWords = words.map((word) => {
+    const middle = Math.ceil(word.length / 2)
+    const firstHalf = word.slice(0, middle)
+    const secondHalf = word.slice(middle)
+    return secondHalf + firstHalf
+  })
+  return transformedWords.join(' ')
+}
+
+const input = process.argv[2]
+const result = verydisco(input)
 
 fs.writeFile('verydisco-forever.txt', result, (err) => {
   if (err) {
