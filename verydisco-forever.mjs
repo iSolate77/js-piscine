b3e896985d7a7606f5891968a8b5694dcfffa508
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-28 17:59:55 +0300

    why

--- verydisco-forever.mjs
+++ verydisco-forever.mjs
@@ -5,14 +5,24 @@ const verydisco = (input) => {
   const words = input
     .match(/"[^"]+"|\S+/g)
     .map((word) => word.replace(/"/g, ''))
-  const transformedWords = words.map((word) => {
-    const middle = Math.ceil(word.length / 2)
-    const firstHalf = word.slice(0, middle)
-    const secondHalf = word.slice(middle)
-    return secondHalf + firstHalf
+  const transformedWords = words.reverse().map((word) => {
+    return word.split('').reverse().join('')
   })
   return transformedWords.join(' ')
 }
+
+// const verydisco = (input) => {
+//   const words = input
+//     .match(/"[^"]+"|\S+/g)
+//     .map((word) => word.replace(/"/g, ''))
+//   const transformedWords = words.map((word) => {
+//     const middle = Math.ceil(word.length / 2)
+//     const firstHalf = word.slice(0, middle)
+//     const secondHalf = word.slice(middle)
+//     return secondHalf + firstHalf
+//   })
+//   return transformedWords.join(' ')
+// }
 
 const input =
   process.argv.length > 3 ? process.argv.slice(2).join('') : process.argv[2]
