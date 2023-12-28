979207764560983818c130ec6aef678aaf91e2b4
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-28 17:55:33 +0300

    why you torture me like this

---
+++ verydisco-forever.js
@@ -0,0 +1,26 @@
+import process from 'process';
+import fs from 'fs';
+
+const verydisco = (input) => {
+  const words = input
+    .match(/"[^"]+"|\S+/g)
+    .map((word) => word.replace(/"/g, ''))
+  const transformedWords = words.map((word) => {
+    const middle = Math.ceil(word.length / 2)
+    const firstHalf = word.slice(0, middle)
+    const secondHalf = word.slice(middle)
+    return secondHalf + firstHalf
+  })
+  return transformedWords.join(' ')
+}
+
+const input = process.argv.length > 3 ? process.argv.slice(2).join(' ') : process.argv[2];
+const result = verydisco(input)
+
+fs.writeFile('verydisco-forever.txt', result, (err) => {
+  if (err) {
+    console.error('Error writing to file:', err)
+  } else {
+    console.log('Result written to verydisco-forever.txt')
+  }
+})
