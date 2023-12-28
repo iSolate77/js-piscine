4cf0d5f9ca2b7c82af8fd1290fdddda679a87f64
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-28 17:15:50 +0300

    async

--- get-json.js
+++ get-json.js
@@ -1,22 +1,23 @@
-async function getJSON(path, params = {}, baseUrl = 'http://localhost') {
-  const url = new URL(path, baseUrl)
-
-  Object.keys(params).forEach((key) =>
-    url.searchParams.append(key, params[key]),
-  )
-
-  try {
-    const response = await fetch(url)
-    if (!response.ok) {
+async function getJSON(path = '', params = {}) {
+  const url =
+    path +
+    '?' +
+    Object.keys(params)
+      .map((key) => {
+        return (
+          key.replace(' ', '+') + '=' + params[key].toString().replace(' ', '+')
+        )
+      })
+      .join('&')
+  const res = await fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json()
+    } else {
       throw new Error(response.statusText)
     }
-    const parsedResponse = await response.json()
-
-    if (parsedResponse.error) {
-      throw new Error(parsedResponse.error)
-    }
-    return parsedResponse.data
-  } catch (error) {
-    throw error
+  })
+  if (res.error) {
+    throw new Error(res.error)
   }
+  return res.data
 }
