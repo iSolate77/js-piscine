76510c39f4e1dd74ea6712faa57cc59b09df502d
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-28 18:43:58 +0300

    why

--- tell-me-vip.mjs
+++ tell-me-vip.mjs
@@ -1,8 +1,5 @@
-import { promises } from 'fs'
+import { promises as fs } from 'fs'
 import { join } from 'path'
-
-const filePath = join(__dirname, 'guests.json')
-const vipPath = join(__dirname, 'vip.txt')
 
 const formatGuest = (guest, index) =>
   `${index + 1}. ${guest.lastname.toUpperCase()} ${guest.firstname}`
@@ -12,10 +9,32 @@ const formatGuests = (guests) => guests.map(formatGuest).join('\n')
 const filterGuests = (guests) =>
   guests.filter((guest) => guest.answer === 'YES')
 
-promises
-  .readFile(filePath, 'utf-8')
-  .then((content) => JSON.parse(content))
-  .then(filterGuests)
-  .then(formatGuests)
-  .then((content) => promises.writeFile(vipPath, content))
-  .catch((err) => console.error(err.message))
+const readGuestsFromDir = async (dirPath) => {
+  const fileNames = await fs.readdir(dirPath)
+  const guestPromises = fileNames.map((fileName) =>
+    fs.readFile(join(dirPath, fileName), 'utf-8').then(JSON.parse),
+  )
+  return Promise.all(guestPromises)
+}
+
+const main = async (dirName) => {
+  const dirPath = join(__dirname, dirName)
+  const vipPath = join(__dirname, 'vip.txt')
+
+  try {
+    const guests = await readGuestsFromDir(dirPath)
+    const vipGuests = filterGuests(guests)
+    const formattedGuests = formatGuests(vipGuests)
+    await fs.writeFile(vipPath, formattedGuests)
+  } catch (err) {
+    console.error(err.message)
+  }
+}
+
+const dirName = process.argv[2]
+if (!dirName) {
+  console.error('Please provide a directory name as an argument.')
+  process.exit(1)
+}
+
+main(dirName)
