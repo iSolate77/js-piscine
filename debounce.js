e2841fc5c647b784c1e451687ea0ec9bc2dc3937
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-28 16:41:40 +0300

    why o why

--- debounce.js
+++ debounce.js
@@ -1,40 +1,34 @@
-function debounce(func, wait) {
-  let timeout
-
-  return function executedFunction(...args) {
-    const later = () => {
-      clearTimeout(timeout)
-      func(...args)
-    }
-
-    clearTimeout(timeout)
-    timeout = setTimeout(later, wait)
+function debounce(fn, delay) {
+  let timer = null
+  return function() {
+    let context = this
+    let args = arguments
+    clearTimeout(timer)
+    timer = setTimeout(function() {
+      fn.apply(context, args)
+    }, delay)
   }
 }
 
-function opDebounce(func, wait, options = {}) {
-  let timeout
-  let lastInvocationTime
-  let result
-
-  return function executedFunction(...args) {
-    const now = Date.now()
-    const isLeadingCall =
-      options.leading &&
-      (!lastInvocationTime || now - lastInvocationTime >= wait)
-
-    if (isLeadingCall) {
-      lastInvocationTime = now
-      result = func(...args)
-    } else if (!timeout) {
-      timeout = setTimeout(() => {
-        lastInvocationTime = options.leading ? Date.now() : null
-        timeout = null
-        if (!options.leading || lastInvocationTime) {
-          result = func(...args)
-        }
-      }, wait)
+function opDebounce(fn, delay, options) {
+  var timer = null,
+    first = true,
+    leading
+  if (typeof options === 'object') {
+    leading = !!options.leading
+  }
+  return function() {
+    let context = this,
+      args = arguments
+    if (first && leading) {
+      fn.apply(context, args)
+      first = false
     }
-    return result
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(function() {
+      fn.apply(context, args)
+    }, delay)
   }
 }
