8290445ee7bb7cfa13d60233ef85e1f67597c2ae
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-28 18:22:28 +0300

    why

--- verydisco-reverso.mjs
+++ verydisco-reverso.mjs
@@ -3,25 +3,12 @@ import fs from 'fs/promises'
 async function decipherVeryDisco(filename) {
   try {
     const content = await fs.readFile(filename, 'utf8')
-    const deciphered = reverseVeryDisco(content)
+    const deciphered = content.split('').reverse().join('')
 
     console.log(deciphered)
   } catch (error) {
     console.error('Error reading the file:', error.message)
   }
-}
-
-function reverseVeryDisco(str) {
-  const midIndex = Math.floor(str.length / 2)
-  const isOddLength = str.length % 2 !== 0
-  const firstHalf = [...str.slice(0, midIndex)].reverse().join('')
-  const secondHalf = [...str.slice(isOddLength ? midIndex + 1 : midIndex)]
-    .reverse()
-    .join('')
-
-  return isOddLength
-    ? firstHalf + str[midIndex] + secondHalf
-    : firstHalf + secondHalf
 }
 
 const filename = process.argv[2]
