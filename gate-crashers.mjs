03b5a2a61333df41f3b86687c1dc0fd0937c23af
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-28 19:15:13 +0300

    why

---
+++ gate-crashers.mjs
@@ -0,0 +1,61 @@
+import http from 'http'
+import { promises as fs } from 'fs'
+import path from 'path'
+
+const port = 5000
+const authorizedUsers = {
+  Caleb_Squires: 'abracadabra',
+  Tyrique_Dalton: 'abracadabra',
+  Rahima_Young: 'abracadabra',
+}
+
+const server = http.createServer(async (req, res) => {
+  try {
+    res.setHeader('Content-Type', 'application/json')
+
+    if (req.method === 'POST') {
+      const authHeader = req.headers['authorization']
+      if (!authHeader || !isAuthorized(authHeader)) {
+        res.writeHead(401)
+        res.end(JSON.stringify({ error: 'Unauthorized' }))
+        return
+      }
+
+      let body = ''
+      req.on('data', (chunk) => {
+        body += chunk.toString()
+      })
+
+      req.on('end', async () => {
+        try {
+          const guestName = req.url.slice(1)
+          const filePath = path.join('./guests', `${guestName}.json`)
+          await fs.writeFile(filePath, body, 'utf8')
+          res.writeHead(201)
+          res.end(body)
+        } catch (err) {
+          res.writeHead(500)
+          res.end(JSON.stringify({ error: 'server failed' }))
+        }
+      })
+    } else {
+      res.writeHead(500)
+      res.end(JSON.stringify({ error: 'server failed' }))
+    }
+  } catch (err) {
+    res.writeHead(500)
+    res.end(JSON.stringify({ error: 'server failed' }))
+  }
+})
+
+function isAuthorized(authHeader) {
+  const token = authHeader.split(' ')[1]
+  const [username, password] = Buffer.from(token, 'base64')
+    .toString('utf8')
+    .split(':')
+  return authorizedUsers[username] === password
+}
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
