8dc2905335eb027bb83cfa3db51963b31d954e11
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-28 16:58:55 +0300

    async

--- interpolation.js
+++ interpolation.js
@@ -1,13 +1,14 @@
 function interpolation({ step, start, end, callback, duration }) {
-  const deltaX = (end[0] - start[0]) / step
-  const deltaY = (end[1] - start[1]) / step
-  const delay = duration / step
-
-  for (let i = 0; i < step; i++) {
-    setTimeout(() => {
-      const x = start[0] + deltaX * i
-      const y = start[1] + deltaY * i
-      callback([x, y])
-    }, delay * i)
-  }
+  const delta = (end - start) / step
+  let current = start
+  let i = 0
+  const timer = setInterval(() => {
+    if (i < step) {
+      callback([current, (duration / step) * (i + 1)])
+      current += delta
+      i++
+    } else {
+      clearInterval(timer)
+    }
+  }, duration / step)
 }
