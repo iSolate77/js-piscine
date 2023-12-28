29d73ead0cbfa35c011f3adafb0c9941737ea8d3
Mohamed Faris <mohd.faris07@gmail.com>
2023-12-28 17:07:42 +0300

    async

--- keep-trying-or-giveup.js
+++ keep-trying-or-giveup.js
@@ -1,15 +1,28 @@
-function retry(count, callback) {
+function retry(count = 3, callback = async () => {}) {
   return async function (...args) {
-    let attempts = 0
-    while (attempts <= count) {
-      try {
-        return await callback(...args)
-      } catch (error) {
-        if (attempts === count) {
-          throw new Error('Maximum retry attempts reached')
-        }
-        attempts++
+    try {
+      const res = await callback(...args)
+      return res
+    } catch (e) {
+      if (count > 0) {
+        return retry(count - 1, callback)(...args)
+      } else {
+        throw e
       }
     }
+  }
+}
+
+function timeout(delay = 0, callback = async () => {}) {
+  return async function (...args) {
+    const timeout = new Promise((resolve) =>
+      setTimeout(resolve, delay, Error('timeout')),
+    )
+    const functionCall = new Promise((resolve) => resolve(callback(...args)))
+    const res = await Promise.race([timeout, functionCall]).then((res) => res)
+    if (res instanceof Error) {
+      throw res
+    }
+    return res
   }
 }
